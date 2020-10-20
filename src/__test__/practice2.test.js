ebfa1e9c01276b228cc83e3c25e9fdfc5d92bb53
GraceeFlower <1491644103@qq.com>
2020-10-20 09:20:37 +0800

    [Min] refactor: practice2

--- src/__test__/practice2.test.js
+++ src/__test__/practice2.test.js
@@ -1,5 +1,4 @@
-import { Person } from "../practice2/Person";
-import { Student } from "../practice2/Student";
+import { Person, Student } from "../practice2";
 
 const kevin = new Person('Kevin')
 const winnie = new Student('Winnie', 'class1')
